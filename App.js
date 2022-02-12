76a67cfbfca716a2d949a141833b01e08efd2659
NguyenTranMinhTrang <97238264+NguyenTranMinhTrang@users.noreply.github.com>
2022-02-12 22:34:15 +0700

    set up tabbar done 26:5

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native"
+import { Home, Restaurant, OrderDilivery } from "./screens/index";
+import Tabs from "./navigation/tabs";
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false
+        }}
+        initialRouteName={Home}
+      >
+        <Stack.Screen name="Home" component={Tabs} />
+        <Stack.Screen name="Restaurant" component={Restaurant} />
+        <Stack.Screen name="OrderDilivery" component={OrderDilivery} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ screens/index.js
@@ -0,0 +1,5 @@
+import Home from "./home";
+import Restaurant from "./restaurant";
+import OrderDilivery from "./orderDelivery";
+
+export { Home, Restaurant, OrderDilivery };
