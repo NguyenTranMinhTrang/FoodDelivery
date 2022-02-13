76c28b2665dfffa7b02cd3e97f1dc349c528bef8
NguyenTranMinhTrang <97238264+NguyenTranMinhTrang@users.noreply.github.com>
2022-02-13 12:00:54 +0700

    Home Screen 42:26

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native"
 import { Home, Restaurant, OrderDilivery } from "./screens/index";
 import Tabs from "./navigation/tabs";
+import { useFonts } from 'expo-font';
+import AppLoading from "expo-app-loading";
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+  let [fontsLoaded] = useFonts({
+    'Roboto-Bold': require('./assets/fonts/Roboto-Bold.ttf'),
+    'Roboto-Regular': require('./assets/fonts/Roboto-Regular.ttf'),
+    'Roboto-Black': require('./assets/fonts/Roboto-Black.ttf'),
+  })
+
+  if (!fontsLoaded) {
+    return (
+      <AppLoading />
+    );
+  }
+
   return (
     <NavigationContainer>
       <Stack.Navigator
