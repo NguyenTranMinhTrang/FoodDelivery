22eaccbf55365662e7b1967a3259783e517f7a38
NguyenTranMinhTrang <97238264+NguyenTranMinhTrang@users.noreply.github.com>
2022-02-14 15:45:39 +0700

    Restaurant Done

--- screens/restaurant.js
+++ screens/restaurant.js
@@ -8,12 +8,62 @@ const Restaurant = ({ route, navigation }) => {
     const scrollX = new Animated.Value(0);
     const [restaurant, setRestaurant] = React.useState(null);
     const [currentLocation, setCurrentLocation] = React.useState(null);
+    const [orderItem, setOrderItem] = React.useState([]);
 
     React.useEffect(() => {
         let { item, location } = route.params;
         setRestaurant(item);
         setCurrentLocation(location);
     });
+
+    function editOrder(action, menuId, price) {
+        let orderList = orderItem.slice();
+        let item = orderList.filter(a => a.menuId == menuId);
+
+        if (action == "+") {
+            if (item.length > 0) {
+                let newQty = item[0].qty + 1;
+                item[0].qty = newQty;
+                item[0].total = item[0].qty * price;
+            } else {
+                const newItem = {
+                    menuId: menuId,
+                    qty: 1,
+                    price: price,
+                    total: price
+                }
+                orderList.push(newItem);
+            }
+            setOrderItem(orderList);
+        } else {
+            if (item.length > 0) {
+                if (item[0]?.qty > 0) {
+                    let newQty = item[0].qty - 1;
+                    item[0].qty = newQty;
+                    item[0].total = item[0].qty * price;
+                }
+            }
+            setOrderItem(orderList);
+        }
+    }
+
+    function getOrderQuantity(menuId) {
+        let orderItems = orderItem.filter(a => a.menuId == menuId);
+        if (orderItems.length > 0) {
+            return orderItems[0].qty;
+        }
+        return 0;
+    }
+
+    function getTotal() {
+        let total = orderItem.reduce((a, b) => a + (b.qty || 0), 0);
+        return total;
+    }
+
+    function sumOrder() {
+        let total = orderItem.reduce((a, b) => a + (b.total || 0), 0);
+        return total.toFixed(2);
+    }
 
     function renderHeader() {
         return (
@@ -128,6 +178,7 @@ const Restaurant = ({ route, navigation }) => {
                                             borderBottomLeftRadius: SIZES.radius,
                                             borderTopLeftRadius: SIZES.radius
                                         }}
+                                        onPress={() => editOrder("-", item.menuId, item.price)}
                                     >
                                         <Text style={{ ...FONTS.body1 }}>-</Text>
                                     </TouchableOpacity>
@@ -139,7 +190,7 @@ const Restaurant = ({ route, navigation }) => {
                                             alignItems: "center",
                                         }}
                                     >
-                                        <Text style={{ ...FONTS.h2 }}>5</Text>
+                                        <Text style={{ ...FONTS.h2 }}>{getOrderQuantity(item.menuId)}</Text>
                                     </View>
                                     <TouchableOpacity
                                         style={{
@@ -151,6 +202,7 @@ const Restaurant = ({ route, navigation }) => {
                                             borderBottomRightRadius: SIZES.radius,
                                             borderTopRightRadius: SIZES.radius
                                         }}
+                                        onPress={() => editOrder("+", item.menuId, item.price)}
                                     >
                                         <Text style={{ ...FONTS.body1 }}>+</Text>
                                     </TouchableOpacity>
@@ -267,9 +319,77 @@ const Restaurant = ({ route, navigation }) => {
                             borderBottomWidth: 1
                         }}
                     >
-
+                        <Text style={{ ...FONTS.h3 }}>{getTotal()} items in Cart</Text>
+                        <Text style={{ ...FONTS.h3 }}>${sumOrder()}</Text>
+                    </View>
+                    <View
+                        style={{
+                            flexDirection: "row",
+                            justifyContent: "space-between",
+                            paddingVertical: SIZES.padding * 2,
+                            paddingHorizontal: SIZES.padding * 3,
+                        }}
+                    >
+                        <View style={{ flexDirection: "row", }}>
+                            <Image
+                                source={icons.pin}
+                                resizeMode="contain"
+                                style={{
+                                    height: 20,
+                                    width: 20,
+                                    tintColor: COLORS.darkgray
+                                }}
+                            />
+                            <Text style={{ marginLeft: SIZES.padding, ...FONTS.h3 }}>Location</Text>
+                        </View>
+                        <View style={{ flexDirection: "row", }}>
+                            <Image
+                                source={icons.master_card}
+                                resizeMode="contain"
+                                style={{
+                                    height: 20,
+                                    width: 20,
+                                    tintColor: COLORS.darkgray
+                                }}
+                            />
+                            <Text style={{ marginLeft: SIZES.padding, ...FONTS.h3 }}>1508</Text>
+                        </View>
+                    </View>
+                    {/* Order button */}
+                    <View
+                        style={{
+                            padding: SIZES.padding * 2,
+                            alignItems: "center",
+                            justifyContent: "center",
+                        }}
+                    >
+                        <TouchableOpacity
+                            style={{
+                                padding: SIZES.padding,
+                                alignItems: "center",
+                                width: SIZES.width * 0.9,
+                                backgroundColor: COLORS.primary,
+                                borderRadius: SIZES.radius
+                            }}
+                        >
+                            <Text style={{ ...FONTS.h2, color: COLORS.white }}>Order</Text>
+                        </TouchableOpacity>
                     </View>
                 </View>
+                {isIphoneX() &&
+                    <View
+                        style={{
+                            backgroundColor: COLORS.white,
+                            position: 'absolute',
+                            bottom: -34,
+                            left: 0,
+                            right: 0,
+                            height: 34,
+                        }}
+                    >
+
+                    </View>
+                }
             </View>
         )
     }
